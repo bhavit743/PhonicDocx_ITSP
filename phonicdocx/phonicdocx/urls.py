648d4aba47ab89ec3392fb6ce1793b160b7d8c3f
bhavit743 <83495567+bhavit743@users.noreply.github.com>
2021-07-18 18:16:01 +0530

    Update urls.py

--- phonicdocx/phonicdocx/urls.py
+++ phonicdocx/phonicdocx/urls.py
@@ -23,7 +23,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^eel/', include('django_eel.urls')),
-    path('phonicdocx/', include('myapp.urls') ),
+    path('', include('myapp.urls') ),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
