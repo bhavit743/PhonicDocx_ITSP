b5bb9281a1360a6236f4eff540e6800f2f6d6e1e
Bhavit Gupta <bhavitgupta743@gmail.com>
2021-07-18 23:25:38 +0530

    updated1

--- myapp/views.py
+++ myapp/views.py
@@ -7,8 +7,8 @@ def index(request):
         user_input = request.POST.get('inp')
         data = userinp(user_input = user_input)
         data.save()
-    request.session['user_input'] = user_input
     user_input = swap(user_input)
+    request.session['user_input'] = user_input
     # userf = swap.swap(user_input)
     return render(request, "home.html", {'userd': user_input});
 
