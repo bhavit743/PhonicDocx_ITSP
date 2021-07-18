a6b4bfda918a4e48917d05770fa72e2a4f005598
Bhavit Gupta <bhavitgupta743@gmail.com>
2021-07-18 18:34:58 +0530

    dep3

--- phonicdocx/phonicdocx/settings.py
+++ phonicdocx/phonicdocx/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = ['static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
